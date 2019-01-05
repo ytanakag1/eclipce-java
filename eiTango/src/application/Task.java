caf04330d8cefe3f313b161c65139c78887ef739
ytanakag1 <ytanakag1@gmail.com>
2019-01-05 23:29:50 +0900

    initial commit

---
+++ eiTango/src/application/Task.java
@@ -0,0 +1,28 @@
+package application;
+
+public class Task {
+	private String tgtText;
+	
+	// コンストラクタ
+	public Task(String tgt_text) {
+		tgtText=tgt_text;
+	}
+	
+	
+	// テキストBOXの値処理
+	public String getResultText(String ans) {
+		String resultText = "";
+		// 正解判定
+		if (tgtText.equals(ans)) {
+			resultText = "正解!";
+		}else {
+			resultText = "ハズレ";
+		}
+		return resultText;
+	}
+	
+	
+	// 問題をつくる
+	
+	
+}
